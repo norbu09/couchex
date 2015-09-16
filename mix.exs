f2d0b8fc2fed5b148b6193d2ec0a5b21dfb58461
Lenz Gschwendtner <lenz@springtimesoft.com>
2015-09-16 17:27:21 +1200

    fixed missing apps

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Couchex.Mixfile do
 
   def project do
     [app: :couchex,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -14,7 +14,7 @@ defmodule Couchex.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger],
+    [applications: [:logger, :hackney, :poison],
      mod: {Couchex, []}]
   end
 
