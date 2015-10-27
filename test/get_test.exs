715508a119d1546a87574c6d24585bca461b22e7
Rob Ramsay <robert.ramsay.nz@gmail.com>
2015-10-27 16:42:45 +1300

    add basic valid database checking to tests
    Ideally this would also check for existence of views and
    data used by the tests

--- test/get_test.exs
+++ test/get_test.exs
@@ -18,18 +18,21 @@ defmodule CouchexGetTest do
 # }
 
     test "add a document" do
+      database_checks!
       doc = %{"foo" => "bar", "number" => 5, "float" => 3.1415, "array" => ["bla", "blubb"]}
       {:ok, res} = Couchex.Client.put("test", doc)
       assert res["ok"] == true
     end
 
     test "get a document" do
+      database_checks!
       id = "bla"
       {:ok, doc} = Couchex.Client.get("test", id)
       assert id == doc["_id"]
     end
 
     test "update a document" do
+      database_checks!
       {:ok, doc} = Couchex.Client.get("test", "bla")
       doc1 = %{doc | "pid" => :erlang.pid_to_list(self())}
       {:ok, res} = Couchex.Client.put("test", doc1)
@@ -37,6 +40,7 @@ defmodule CouchexGetTest do
     end
 
     test "delete a document" do
+      database_checks!
       doc = %{"foo" => "bar"}
       {:ok, res1} = Couchex.Client.put("test", doc)
       # {:ok,
@@ -48,11 +52,13 @@ defmodule CouchexGetTest do
     end
 
     test "get view" do
+      database_checks!
       {:ok, view} = Couchex.Client.get("test", %{view: "foo/bar", long: true})
       assert view["offset"] == 0
     end
 
     test "get view as a key based hash" do
+      database_checks!
       doc = %{"foo" => "foobar"}
       {:ok, res} = Couchex.Client.put("test", doc)
       {:ok, view} = Couchex.Client.get("test", %{view: "foo/bar", key_based: true})
@@ -61,6 +67,7 @@ defmodule CouchexGetTest do
     end
 
     test "get reduce" do
+      database_checks!
       key = "bar"
       # a reduce retruns a list!
       {:ok, [view]} = Couchex.Client.get("test", %{view: "foo/reduce"}, %{"group" => true, "key" => "bar"})
@@ -68,20 +75,36 @@ defmodule CouchexGetTest do
     end
 
     test "list all databases" do
+      database_checks!
       {:ok, list} = Couchex.Client.all_dbs
       assert is_list(list), "#{inspect list} is not a list"
       assert Enum.member?(list, "_users"), "#{inspect list} should contain \"_users\""
     end
 
     test "create a database" do
+      database_checks!
       {:ok, res} = Couchex.Client.create_db("test_db_for_db_creation")
       assert res["ok"] == true
       {:ok, _res} = Couchex.Client.delete_db("test_db_for_db_creation")
     end
 
     test "delete a database" do
+      database_checks!
       {:ok, _res} = Couchex.Client.create_db("test_db_for_db_deletion")
       {:ok, res} = Couchex.Client.delete_db("test_db_for_db_deletion")
       assert res["ok"] == true
+    end
+
+    defp database_checks! do
+      {:ok, db_list} = Couchex.Client.all_dbs
+
+      if ! Enum.member?(db_list, "test") do
+        raise "CouchDB database missing"
+      end
+
+      if  Enum.member?(db_list, "test_db_for_db_creation") ||
+          Enum.member?(db_list, "test_db_for_db_deletion") do
+        raise "CouchDB database not cleaned up after last test"
+      end
     end
 end
