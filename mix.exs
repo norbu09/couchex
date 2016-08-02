fe7d9c2cd4ed8bce76a1f7e12f049db860948869
Lenz Gschwendtner <lenz@springtimesoft.com>
2016-08-02 19:28:49 +1200

    bump version number

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Couchex.Mixfile do
 
   def project do
     [app: :couchex,
-     version: "0.0.7",
+     version: "0.0.8",
      elixir: ">= 1.0.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
